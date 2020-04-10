466afdd5c5ea963ba4891936ab125d7e061d4abf
Lizzy Chen <lizzychen@Lizzys-MacBook-Pro.local>
2020-04-09 17:17:06 -0700

    second push

--- app/src/main/java/com/ljchen17/myapplication/MainActivity.kt
+++ app/src/main/java/com/ljchen17/myapplication/MainActivity.kt
@@ -33,14 +33,23 @@ class MainActivity : AppCompatActivity() {
 
             val typeInUserName = findViewById<EditText>(R.id.editUserName)
             val userInputValue = findViewById<TextView>(R.id.userInputValue)
-            userInputValue.text = typeInUserName.text.toString()
-            userInputValue.visibility = View.VISIBLE
-            typeInUserName.visibility = View.INVISIBLE
 
-            val changeButtonName = findViewById<Button>(R.id.changeUserName)
-            changeButtonName.text = "CHANGE USER"
+            if (typeInUserName.text.toString() == "") {
+                val text = "Please type in a username"
+                val duration = Toast.LENGTH_SHORT
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
+                toast.setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 0)
+            } else {
 
-            editMode = false
+                val changeButtonName = findViewById<Button>(R.id.changeUserName)
+                userInputValue.text = typeInUserName.text.toString()
+                userInputValue.visibility = View.VISIBLE
+                typeInUserName.visibility = View.INVISIBLE
+                changeButtonName.text = "CHANGE USER"
+                editMode = false
+            }
+
 
         } else {
 
@@ -57,28 +66,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-        fun clickPlay(view: View) {
-            randomNumber += 1
-            val playTimes = findViewById<TextView>(R.id.playTimes)
-            playTimes.text = "$randomNumber plays"
-        }
+    fun clickPlay(view: View) {
+        randomNumber += 1
+        val playTimes = findViewById<TextView>(R.id.playTimes)
+        playTimes.text = "$randomNumber plays"
+    }
 
-        fun clickPrevious(view: View) {
-            val text = "Skipping to previous track"
-            val duration = Toast.LENGTH_SHORT
-            val toast = Toast.makeText(applicationContext, text, duration)
-            toast.show()
-            toast.setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 0)
-        }
+    fun clickPrevious(view: View) {
+        val text = "Skipping to previous track"
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
+        toast.setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 0)
+    }
 
-        fun clickNext(view: View) {
-            val text = "Skipping to next track"
-            val duration = Toast.LENGTH_SHORT
-            val toast = Toast.makeText(applicationContext, text, duration)
-            toast.show()
-            toast.setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 0)
-        }
-
+    fun clickNext(view: View) {
+        val text = "Skipping to next track"
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
+        toast.setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 0)
+    }
 
 
 }
